783cfcfffcce39511a32fdc2d6fe59ffe4bd3fb1
seungchan <vb1201@naver.com>
2022-05-18 10:06:20 +0900

    textfilewriter

---
+++ textfilewriter.c
@@ -0,0 +1,13 @@
+void create_music_titles(FILE* stream){
+
+}
+void read_file(char* file_name){
+	FILE *stream;
+	stream = fopen(file_name, "r");
+}
+void write_file(char* file_name){
+	FILE *stream;
+	stream = f
+}
+
+
