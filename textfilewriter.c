2762c2800d458d93dcf996580c97728211396b3c
seungchan <vb1201@naver.com>
2022-05-23 09:43:03 +0900

    textfilewriter.c

--- textfilewriter.c
+++ textfilewriter.c
@@ -4,10 +4,13 @@ void create_music_titles(FILE* stream){
 void read_file(char* file_name){
 	FILE *stream;
 	stream = fopen(file_name, "r");
+	create_music_titles(steam);
 }
 void write_file(char* file_name){
 	FILE *stream;
-	stream = f
+	stream = fopen(file_name, "w");
+	fputs( , stream);
+	fclose(stream);
 }
 
 
