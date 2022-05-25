215796fe07efbe491ecb6a9a626a43afe542a4fb
seungchan <vb1201@naver.com>
2022-05-25 11:23:21 +0900

    read_file() update

--- libs/textfilewriter.c
+++ libs/textfilewriter.c
@@ -2,16 +2,17 @@ void create_music_titles(FILE* stream){
 
 }
 void read_file(char* file_name){
-	FILE *stream;
-	stream = fopen(file_name, "r");
+	FILE *stream = fopen(file_name, "r");
 	int music_cnt;
 	fscanf(stream, "%d", &music_cnt);
+	fgetc(stream);
 	char music_titles[music_cnt][100];
         for (int i=0; i < music_cnt; i++){
                 fgets(music_titles[i], 100, stream);
         }
 	int instr_cnt;
 	fscanf(stream, "%d", &instr_cnt);
+	fgetc(stream);
 	char instr_set[instr_cnt][100];
         for (int i=0; i < instr_cnt; i++){
                 fgets(instr_set[i], 100, stream);
@@ -19,8 +20,7 @@ void read_file(char* file_name){
 	//create_music_titles(steam);
 }
 void write_file(char* file_name){
-	FILE *stream;
-	stream = fopen(file_name, "w");
+	FILE *stream = fopen(file_name, "w");
 	fputs( , stream);
 	fclose(stream);
 }
