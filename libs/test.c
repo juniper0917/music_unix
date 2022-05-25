866dbdd8a85a089c356c50cd39655129f9574781
seungchan <vb1201@naver.com>
2022-05-25 10:48:47 +0900

    text_test

---
+++ libs/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	FILE *stream = fopen("test_text.txt", "r");
+        int music_cnt;
+        fscanf(stream, "%d", &music_cnt);
+        char music_titles[music_cnt][100];
+        for (int i=0; i < music_cnt; i++){
+                fgets(music_titles[i], 100, stream);
+		printf("%s", music_titles[i]);
+        }
+        int instr_cnt;
+        fscanf(stream, "%d", &instr_cnt);
+        char instr_set[instr_cnt][100];
+        for (int i=0; i < instr_cnt; i++){
+                fgets(instr_set[i], 100, stream);
+		printf("%s", instr_set[i]);
+        }
+
+	return 0;
+}
--- libs/textfilewriter.c
+++ libs/textfilewriter.c
@@ -4,17 +4,19 @@ void create_music_titles(FILE* stream){
 void read_file(char* file_name){
 	FILE *stream;
 	stream = fopen(file_name, "r");
-	int music_count = (int)fgets();
-	char* music_titles[] = new char*[music_count];
-        for (int i=0; i < music_count; i++){
-                music_titles[i] = fgets();
+	int music_cnt;
+	fscanf(stream, "%d", &music_cnt);
+	char music_titles[music_cnt][100];
+        for (int i=0; i < music_cnt; i++){
+                fgets(music_titles[i], 100, stream);
         }
-	int instr_cnt = (int)fgets();
-	char* instr_set[] = new char*[instr_cnt];
+	int instr_cnt;
+	fscanf(stream, "%d", &instr_cnt);
+	char instr_set[instr_cnt][100];
         for (int i=0; i < instr_cnt; i++){
-                instr_set[i] = fgets();
+                fgets(instr_set[i], 100, stream);
         }
-	create_music_titles(steam);
+	//create_music_titles(steam);
 }
 void write_file(char* file_name){
 	FILE *stream;
