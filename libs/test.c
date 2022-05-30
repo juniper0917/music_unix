22c1651063fa8576cc00a66221fb263c63221085
seungchan <vb1201@naver.com>
2022-05-30 10:07:29 +0900

    connect linkedlist to textfilewriter

--- libs/test.c
+++ libs/test.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_TITLE_SIZE 50
+
+void create_music_titles(FILE* stream){
+        int count=0;
+        char buffer[MAX_TITLE_SIZE];
+        fscanf(stream, "%d", &count);
+        clear();
+        while(count-->0){
+                fscanf(stream, "%s", buffer);
+                append_left(strlen(buffer), buffer);
+        }
+	printf(buffer);
+}
 
 int main(){
 	FILE *stream = fopen("test_text.txt", "r");
+	/*
         int music_cnt;
         fscanf(stream, "%d", &music_cnt);
 	fgetc(stream);
@@ -18,6 +34,10 @@ int main(){
                 fgets(instr_set[i], 100, stream);
 		printf("%s", instr_set[i]);
         }
-
+`	*/
+	create_music_titles(stream);
+	create_music_titles(stream);
+	fclose(stream);
 	return 0;
 }
+
